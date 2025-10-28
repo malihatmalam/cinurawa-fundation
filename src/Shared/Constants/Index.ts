582352f8441be9ff25a623148301975ae815f03b
malihatmalam <55455779+malihatmalam@users.noreply.github.com>
2025-10-28 10:24:46 +0700

    dev-deleted old error message constants

--- src/Shared/Constants/Index.ts
+++ src/Shared/Constants/Index.ts
@@ -1,4 +1,4 @@
-export * from './ErrorMessageConstants.constant';
 export * from './BooleanOperationConstants.constant';
 export * from './FilterOperationConstants.constant';
 export * from './OrderByOperationConstants.constant';
+export * from './ErrorCodeMessageConstants.constant';
