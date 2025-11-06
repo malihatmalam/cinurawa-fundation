c33d58b486d1be2d9cc14e8958d80f82679cd302
malihatmalam <55455779+malihatmalam@users.noreply.github.com>
2025-11-06 11:37:55 +0700

    clean-change name file filter exception

--- src/Exception/index.ts
+++ src/Exception/index.ts
@@ -1,5 +1,5 @@
 export * from './BaseException';
 export * from './DomainException/DomainException';
-export * from './ExceptionFilter';
+export * from './FilterException';
 export * from './ValidationException/IValidationError';
 export * from './ValidationException/ValidationException';
